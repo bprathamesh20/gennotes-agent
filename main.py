285a562f44481008c8464bd55937d60ed35c3f6c
bprathamesh20 <bprathamesh2003@gmail.com>
2025-04-12 14:29:25 +0530

    cors fix

--- main.py
+++ main.py
@@ -139,7 +139,7 @@ origins = [
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"], # Allow all origins for debugging
     allow_credentials=True,
     allow_methods=["*"], # Allows all methods (GET, POST, etc.)
     allow_headers=["*"], # Allows all headers
